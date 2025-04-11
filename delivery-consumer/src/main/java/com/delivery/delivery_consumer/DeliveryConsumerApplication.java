56f5e5cd31850878c2141df48a1e616dbb283090
luisfelipe03 <llipeandrade34@gmail.com>
2025-04-11 09:08:32 -0300

    init: Adicionando os módulos iniciais

---
+++ delivery-consumer/src/main/java/com/delivery/delivery_consumer/DeliveryConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.delivery_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConsumerApplication.class, args);
+	}
+
+}
---
+++ delivery-consumer/src/test/java/com/delivery/delivery_consumer/DeliveryConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.delivery_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
